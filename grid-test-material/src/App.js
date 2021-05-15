efee954f03174d2fbcf3799728c26e40d4addf21
millord <juancarlosmillord@gmail.com>
2021-05-15 18:55:43 -0400

    added heading in the app component

--- grid-test-material/src/App.js
+++ grid-test-material/src/App.js
@@ -28,9 +28,11 @@ color:black;
 
 
 
+
 function App() {
   return (
     <StyledContainer container justify="center">
+      <h1>Testing the Grid System of Material-UI</h1>
       <StyledBox1 className="box">BOX1</StyledBox1>
       <StyledBox2 className="box">BOX2</StyledBox2>
       <StyledBox3 alignItems="center" container direction="column" className="box">BOX3</StyledBox3>
